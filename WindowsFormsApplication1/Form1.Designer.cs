0b54f04facd9254b70ad01b99cc5fad24fd5ce06
BuiNguyenHoangAnh <32384308+BuiNguyenHoangAnh@users.noreply.github.com>
2018-03-04 20:12:04 +0700

    First commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FirstNumber = new System.Windows.Forms.TextBox();
+            this.SecondNumber = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập số thứ nhất";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập số thứ hai";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(55, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kết quả là";
+            // 
+            // FirstNumber
+            // 
+            this.FirstNumber.Location = new System.Drawing.Point(219, 46);
+            this.FirstNumber.Name = "FirstNumber";
+            this.FirstNumber.Size = new System.Drawing.Size(203, 20);
+            this.FirstNumber.TabIndex = 3;
+            // 
+            // SecondNumber
+            // 
+            this.SecondNumber.Location = new System.Drawing.Point(219, 91);
+            this.SecondNumber.Name = "SecondNumber";
+            this.SecondNumber.Size = new System.Drawing.Size(203, 20);
+            this.SecondNumber.TabIndex = 4;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(219, 138);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(203, 20);
+            this.Result.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(43, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "*";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(105, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 30);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "/";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(176, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(245, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(316, 200);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(59, 30);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Xóa";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(382, 200);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(58, 30);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Dừng";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.SecondNumber);
+            this.Controls.Add(this.FirstNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Các phép toán số học";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FirstNumber;
+        private System.Windows.Forms.TextBox SecondNumber;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.FirstNumber.Text.Trim();
+            this.SecondNumber.Text.Trim();
+            int x, y, z;
+            x = Convert.ToInt32(this.FirstNumber.Text);
+            y = Convert.ToInt32(this.SecondNumber.Text);
+            z = x * y;
+            this.Result.Text = Convert.ToString(z);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DialogResult traloi;
+            traloi = MessageBox.Show("Bạn muốn thoát", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (traloi == DialogResult.OK)
+                Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.FirstNumber.Clear();
+            this.SecondNumber.Clear();
+            this.Result.Clear();
+            this.FirstNumber.Focus();
+            this.SecondNumber.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.FirstNumber.Text.Trim();
+            this.SecondNumber.Text.Trim();
+            int x, y, z;
+            x = Convert.ToInt32(this.FirstNumber.Text);
+            y = Convert.ToInt32(this.SecondNumber.Text);
+            z = x / y;
+            this.Result.Text = Convert.ToString(z);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.FirstNumber.Text.Trim();
+            this.SecondNumber.Text.Trim();
+            int x, y, z;
+            x = Convert.ToInt32(this.FirstNumber.Text);
+            y = Convert.ToInt32(this.SecondNumber.Text);
+            z = x + y;
+            this.Result.Text = Convert.ToString(z);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.FirstNumber.Text.Trim();
+            this.SecondNumber.Text.Trim();
+            int x, y, z;
+            x = Convert.ToInt32(this.FirstNumber.Text);
+            y = Convert.ToInt32(this.SecondNumber.Text);
+            z = x - y;
+            this.Result.Text = Convert.ToString(z);
+        }
+    }
+}
